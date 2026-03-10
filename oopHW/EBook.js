7710157c30810797b8497abf1db9683fbb2ad382
MarinaZelenska <osychenkomarina@gmail.com>
2026-03-10 23:25:08 +0200

    added execution file

--- oopHW/EBook.js
+++ oopHW/EBook.js
@@ -39,11 +39,3 @@ export class EBook extends Book{
 
 }
 
-
-const test3 = new EBook('Test3', 'Autor3', 1992, 'pdf');
-test3.title = '1';
-console.log(test3.printInfo());
-
-const test4 = new Book('Book new', 'Autor new', 2026);
-const result = EBook.fromBook(test4, 'txt');
-console.log(result.printInfo());
